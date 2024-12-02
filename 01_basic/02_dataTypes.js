2ad3b60cacd1d24ecdc73517684ffa18bd3af53f
Prashant Singh <93091454+Prashantsingh2000@users.noreply.github.com>
2024-12-02 18:21:11 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+
+//alert(3+3) // we are using nodejs, not browser
+
+
+console.log("prashant")
+
+
+
+
+//  number  => 2 to power 53
+//  bigint  => 
+//  string  => "....."
+//  boolean => true/false
+//  null    => standable value  ( actualy in javascript null is standalone also s datatype in javascript)
+// undefined =>  not assing any value 
+//  symbol   =>  unique
+
+
+
+// Object
+
+
+console.log(typeof undefined); // output => undifined
+console.log(typeof null);    //output => object
