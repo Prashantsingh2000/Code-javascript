2732b08945ccc5c24ae4e54bb4b97c93def59c55
Prashant Singh <93091454+Prashantsingh2000@users.noreply.github.com>
2024-12-02 18:21:30 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1222122
+let acccountEmail = "prashantsingh@gmail.com"
+var accountPassword = "123454"
+accountCity = "jaipur"
+
+console.log(accountId);
+ accountEmail = "raohan@gmail.com"   
+ accountPassword = "122224"
+ accountCity = "Agra"
+ console.table([accountId, accountEmail, accountPassword, accountCity]);
